ed5c86a8554e9445655976d0d76b08ee8dd0aeae
shooming <liomily@gmail.com>
2020-12-02 03:54:11 +0900

    업로드

--- app.js
+++ app.js
@@ -25,19 +25,35 @@ app.get('/', function(req, res){
     })
 })
 
-io.sockets.on('connection', function(socket) { //connection 발생시 콜백함수 실행, io.sockets는 접속되는 모든 소켓들
-    console.log('유저 접속 됨')
+io.sockets.on('connection', function(socket){ //connection 발생시 콜백함수 실행, io.sockets는 접속되는 모든 소켓들
+ 
+    /* 새루운 유저 접속시 다른 소켓에 알림*/
+    socket.on('newUser', function(name){
+        console.log(name + ' 님이 접속하였습니다.')
+        socket.name = name // 소켓에 이름 저장
+        io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + '님이 접속하였습니다.'}) // 모든 소켓에 전송
+    })
 
-    socket.on('send', function(data) { //send라는 이벤트 받을 경우 호출 send는 변수명이기 때문에 자신이 원하는 이벤트 명으로 바꿀 수 있음
+    /* 전송한 메시지 받기 */
+    socket.on('message', function(data){ //message라는 이벤트 받을 경우 호출 message는 변수명이기 때문에 자신이 원하는 이벤트 명으로 바꿀 수 있음
+        data.name = socket.name // 받은 데이터 누가 보낸건지 이름 추가
         console.log(data) // data는 client에서 보내온 json data임
-        console.log('전달된 메시지:', data.msg)
+        socket.broadcast.emit('update', data); // 보낸 사람을 제외한 나머지 유저에게 메시지 전송
     })
 
+    /* 접속 종료 */
     socket.on('disconnect', function() { //disconnect는 socket.io 기본 이벤트 연결된 소켓과 접속 끊기면 자동으로 실행
-        console.log('접속 종료')
+        console.log(socket.name + '님이 나가셨습니다.')
+        /* 나가는 사람을 제외한 나머지 유저에게 메시지 전송 */
+        socket.broadcast.emit('update', {type : 'disconnect', name: 'SERVER', message: socket.name + '님이 나가셨습니다.'});
     })
-
 })
+
+/* 
+메시지 보낼때
+io.sockets.emit() = 모든 유저(본인 포함)
+socket.broadcast.emit() = 본인을 제외한 나머지 모두
+*/
 
 /*서버를 8080 포트로 listen*/
 server.listen(8080, function() {
--- static/js/index.js
+++ static/js/index.js
@@ -2,11 +2,46 @@ var socket = io()
 
 /* 소켓 접속시 실행 */
 socket.on('connect', function() {
-    var input = document.getElementById('test')
-    input.placeholder = '메시지를 입력해 주세요'
+    var name = prompt('반갑습니다!', '') //이름 입력 받고
+    /* 이름이 빈칸인 경우 */
+    if(!name) {
+        name = '익명'
+    }
+
+    socket.emit('newUser', name) // 서버에 새로운 유저 접속 알림
 })
 
-/* 내용 전송 함수 */
+socket.on('update', function(data) {
+    var chat = document.getElementById('chat')
+
+    var message = document.createElement('div')
+    var node = document.createTextNode(`${data.name}: ${data.message}`)
+    var className = ''
+
+
+    // 타입에 따라 적용할 클래스를 다르게 지정
+    switch(data.type) {
+        case 'message':
+        className = 'other'
+        break
+
+        case 'connect':
+        className = 'connect'
+        break
+
+        case 'disconnect':
+        className = 'disconnect'
+        break
+  }
+
+  message.classList.add(className)
+  message.appendChild(node)
+  chat.appendChild(message)
+
+})
+
+
+/* 메시지 전송 함수 */
 function send() {
     // 입력된 데이터 가져옴
     var message = document.getElementById('test').value
@@ -20,5 +55,17 @@ function send() {
     만약 send라는 이름으로 이벤트 전송하면 받는곳에서는 on('send')가 있어야함
     즉 이벤트명이 동일한것 끼지 데이터 송/수신이 이루어짐
     */
-    socket.emit('send', {msg: message})
+
+    // 내가 전송할 메시지 클라이언트에게 표시
+    var chat = document.getElementById('chat')
+    var msg = document.createElement('div')
+    var node = document.createTextNode(message)
+    msg.classList.add('me')
+    msg.appendChild(node)
+    chat.appendChild(msg)
+
+    socket.emit('message', {'type': 'message', message: message})
 }
+
+
+
